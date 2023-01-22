c537692fc8b01e60f8f5a53c05f59263db8c5695
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2023-01-22 04:12:24 -0500

    Update and rename Weather_Converter to Weather-Converter.cpp
    Signed-off-by: Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>

---
+++ Weather-Converter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  
+  int tempf;
+  Int empc;
+  
+  // Ask the user
+  std::cout << "Enter the temperature in Fahrenheit: ";
+  std::cin >> tempf;
+  
+  
+  tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+  /* This program was written in Codeacademy as part of it's C++ progression track. It was an activity using C=++ to convert fahrenheit to celsius and codeacademy gave me
+  the variables and instructions and using that i was able to structure the code as follows */
+
